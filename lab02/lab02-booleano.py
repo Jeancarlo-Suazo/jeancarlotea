3eb0aa4b28b3deb208b9a4f17f674fd3e34fa730
Jeancarlo Suazo Lavaire (24141) <jeancarlo.suazo@est.zamorano.edu>
2022-09-12 20:10:44 -0600

    Version final lab02

---
+++ lab02/lab02-booleano.py
@@ -0,0 +1,18 @@
+#Tendencias e Innovacion en Tecnologia Agricola
+from re import M
+
+
+m = True
+#display m:
+print(m)
+
+#display the data type of m:
+print(type(m))
+
+k = False
+
+#display k:
+print(k)
+
+#display the data type of k:
+print(type(k))
