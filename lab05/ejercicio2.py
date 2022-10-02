1cd5e59a1d6f621abc4dd44dd2178d52141d3e6e
Jeancarlo Suazo Lavaire (24141) <jeancarlo.suazo@est.zamorano.edu>
2022-10-02 14:31:18 -0600

    Laboratorio 5

---
+++ lab05/ejercicio2.py
@@ -0,0 +1,9 @@
+#Tendencias e Innovacion en Tecnologia Agricola 
+try:
+    entrada = input("Ingrese el nombre del archivo: ")
+    archivo = open(entrada)
+    for linea in archivo:
+        print(linea.upper())
+except:
+    print("Error, archivo no existe")
+
